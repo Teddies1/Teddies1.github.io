5fffbc562a26a5e418609439203a94d8c05b5730
Teddies1 <nghonjoo@gmail.com>
2023-06-14 11:24:17 +0800

    disabled javascript for now

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
+/*
 var images = ["../images/street.jpg", "../images/avatar.jpg"]
 
 
 var i = 0;
 setInterval(function(){
     document.body.style.backgroundImage = "url(" + images[i] + ")";
-    console.log("test");
     i = i + 1 % 2;
 }, 5000);
+*/
